48eed8cbce9b2de5f753d7fbdfbc197a2c50516b
Aleksei Anisimov <suxelshock@gmail.com>
2023-02-07 02:22:31 +0300

    Simple pincode example

--- app/src/main/java/ru/practicum/yandex/MainActivity.kt
+++ app/src/main/java/ru/practicum/yandex/MainActivity.kt
@@ -2,40 +2,33 @@ package ru.practicum.yandex
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     companion object {
         const val TAG = "SPRINT_8"
-        const val KEY_TIMER_VALUE = "KEY_TIMER_VALUE"
+        const val KEY_LAST_ON_STOP_TIME = "KEY_LAST_ON_STOP_TIME"
     }
 
-    private var timerTv: TextView? = null
-    private var timerValue = 0
-    private var timer: Timer? = null
+    private var pinCode: TextView? = null
+    private var lastOnStopTime = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "$this onCreate")
         setContentView(R.layout.activity_main)
-        timerTv = findViewById(R.id.timer)
-        timerValue = savedInstanceState?.getInt(KEY_TIMER_VALUE) ?: 0
+        pinCode = findViewById(R.id.pin_code)
+        pinCode?.setOnClickListener {
+            pinCode?.visibility = View.GONE
+        }
+        savedInstanceState?.getLong(KEY_LAST_ON_STOP_TIME)
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG, "$this onStart")
-        timer = Timer().apply {
-            scheduleAtFixedRate(object : TimerTask() {
-                override fun run() {
-                    timerValue++
-                    Log.d(TAG, "$this tick timerValue=$timerValue")
-                    timerTv?.post {
-                        timerTv?.text = timerValue.toString()
-                    }
-                }
-            }, 1000L, 1000L)
+        if (System.currentTimeMillis() - lastOnStopTime > 5000L) {
+            pinCode?.visibility = View.VISIBLE
         }
     }
 
@@ -49,15 +42,10 @@ class MainActivity : AppCompatActivity() {
         Log.d(TAG, "$this onPause")
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt(KEY_TIMER_VALUE, timerValue)
-    }
-
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "$this onStop")
-        timer?.cancel()
+        lastOnStopTime = System.currentTimeMillis()
     }
 
     override fun onDestroy() {
