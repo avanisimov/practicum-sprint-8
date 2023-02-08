7faf0e0e63e0a7482274f1baf44af91a402974ce
Aleksei Anisimov <suxelshock@gmail.com>
2023-02-09 00:02:22 +0300

    Dimens and texts

--- app/src/main/java/ru/yandex/practicum/sprint8/MainActivity.kt
+++ app/src/main/java/ru/yandex/practicum/sprint8/MainActivity.kt
@@ -1,12 +1,12 @@
-package ru.practicum.yandex
+package ru.yandex.practicum.sprint8
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.practicum.yandex.R.layout
+import ru.practicum.yandex.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(layout.activity_main)
+        setContentView(R.layout.activity_main)
     }
 }
